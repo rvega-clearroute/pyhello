9c29f9f9584d124641ca327a8c6fd6d82ddbaec0
rvega-clearroute <rob.vega@clearroute.co.uk>
2023-09-28 11:45:14 +0100

    testing several envs

--- docker/hello.py
+++ docker/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return '<h1>version 1.0.5</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
